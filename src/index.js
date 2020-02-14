543ffc4cc788436104b7f822b931644b4ecba373
Daniel Frye <daniel_frye@autoonemedia.com>
2020-02-13 22:13:19 -0700

    Bedtime Changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AdminLayout from './layouts/Admin.jsx';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/scss/amsurg-style-guide.scss';
+import 'perfect-scrollbar/css/perfect-scrollbar.css';
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+	<Router history={hist}>
+		<Switch>
+			<Route path='/admin' render={props => <AdminLayout {...props} />} />
+			<Redirect to='/admin/dashboard' />
+		</Switch>
+	</Router>,
+	document.getElementById('root')
+);
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+// reactstrap components
+import {
+	Badge,
+	Button,
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	CardTitle,
+	Label,
+	FormGroup,
+	Input,
+	Table,
+	Row,
+	Col,
+	UncontrolledTooltip
+} from 'reactstrap';
+
+class Dashboard extends React.Component {
+	render() {
+		return (
+			<>
+				<div className='content'>
+					<Row>
+						<Col lg='3' md='6' sm='6'>
+							<h1>Sup</h1>
+						</Col>
+					</Row>
+				</div>
+			</>
+		);
+	}
+}
+
+export default Dashboard;
