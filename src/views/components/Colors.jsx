33ac0b205f0323c787ebb4c01d659b06019158be
Frye <Daniel.Frye@EnvisionHealth.com>
2020-03-09 14:27:30 -0600

    End fo work day

--- src/views/components/Colors.jsx
+++ src/views/components/Colors.jsx
@@ -28,32 +28,34 @@ class Colors extends React.Component {
 													return (
 														<div key={key} className='colorContainer'>
 															<h4 className='colorName'>{prop.name}</h4>
-															<Row className='mx-0 colorSquare' style={style}>
-																<Col
-																	xs={3}
-																	className='accessibilityStandards sOne'>
-																	<div className='failA'>A</div>
-																	<div className='failB'>fail</div>
-																</Col>
-																<Col
-																	xs={3}
-																	className='accessibilityStandards sTwo'>
-																	<div className='failA2'>A</div>
-																	<div className='failB2'>fail</div>
-																</Col>
-																<Col
-																	xs={3}
-																	className='accessibilityStandards sThree'>
-																	<div className='PassA'>A</div>
-																	<div className='PassB'>pass</div>
-																</Col>
-																<Col
-																	xs={3}
-																	className='accessibilityStandards sFour'>
-																	<div className='PassA2'>A</div>
-																	<div className='PassB2'>pass</div>
-																</Col>
-															</Row>
+															<div className='mx-0 colorSquare' style={style}>
+																<Row className='mx-0 accessibilityContain'>
+																	<Col
+																		xs={3}
+																		className='accessibilityStandards sOne'>
+																		<div className='failA'>A</div>
+																		<div className='failB'>fail</div>
+																	</Col>
+																	<Col
+																		xs={3}
+																		className='accessibilityStandards sTwo'>
+																		<div className='failA2'>A</div>
+																		<div className='failB2'>fail</div>
+																	</Col>
+																	<Col
+																		xs={3}
+																		className='accessibilityStandards sThree'>
+																		<div className='passA'>A</div>
+																		<div className='passB'>pass</div>
+																	</Col>
+																	<Col
+																		xs={3}
+																		className='accessibilityStandards sFour'>
+																		<div className='passA2'>A</div>
+																		<div className='passB2'>pass</div>
+																	</Col>
+																</Row>
+															</div>
 														</div>
 													);
 												})}
